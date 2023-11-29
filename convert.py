e9d709eabeab41b00cb09362a25aa22b5583c491
theo-baccam <theo.baccam@laplateforme.io>
2023-11-29 14:51:24 +0100

    ajout commande help et list

--- convert.py
+++ convert.py
@@ -4,7 +4,53 @@ from forex_python.converter import CurrencyCodes
 cc = CurrencyCodes()
 
 def prompt_processor(input_string):
+    if input_string == "help":
+        return(
+            "[taux] [devise départ] [devise cible]\n"
+            "`list` pour avoir une liste des devises\n"
+            "Certaines conversions ne pourrait pas marcher car il n'y pas de "
+            "données disponibles."
+        )
+        return ("[taux] [devise départ] [devise cible]\n"
+        "'list' pour avoir une liste des devises'")
     money_list = [value for value in input_string.split(" ")]
+
+    if input_string == "list":
+        return (
+            "Baht thaïlandais: THB\n"
+            "Couronne danoise: DKK\n"
+            "Couronne islandaise: ISK\n"
+            "Couronne norvégienne: NOK\n"
+            "Couronne suédoise: SEK\n"
+            "Couronne thcèque: CZK\n"
+            "Dollar américain: USD\n"
+            "Dollar australien: AUD\n"
+            "Dollar canadien: CAD\n"
+            "Dollar de Hong Kong: HKD\n"
+            "Dollar néo-zélandais: NZD\n"
+            "Dolar singapourien: SGD\n"
+            "Euro: EUR\n"
+            "Forint hongrois: HUF\n"
+            "Franc suisse: CHF\n"
+            "Kuna croate: HRK\n"
+            "Leu roumain: RON\n"
+            "Lev bulgare: BGN\n"
+            "Livre sterling: GBP\n"
+            "Livre turque: TRY\n"
+            "Peso mexicain: MXN\n"
+            "Peso philippin: PHP\n"
+            "Rand sud-africain: ZAR\n"
+            "Real brésilien: BRL\n"
+            "Ringgit malaisien: MYR\n"
+            "Rouble russe: RUB\n"
+            "Roupie indienne: INR\n"
+            "Roupie indonésienne: IDR\n"
+            "Shekel israélien: ILS\n"
+            "Won de la Corée du Sud: KRW\n"
+            "Yen japonais: JPY\n"
+            "Yuan Renminbi chinois: CNY\n"
+            "Zloty polonais: PLN"
+        )
 
     if len(money_list) != 3:
         return "Nombre d'éléments invalide"
