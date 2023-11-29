887fbf38601aa6d404826163f9a4c32e020fe258
theo-baccam <theo.baccam@laplateforme.io>
2023-11-29 14:58:38 +0100

    black

--- convert.py
+++ convert.py
@@ -1,54 +1,59 @@
 from forex_python.converter import CurrencyRates
+
 cr = CurrencyRates()
 from forex_python.converter import CurrencyCodes
+
 cc = CurrencyCodes()
 
 devise_list = [
-            "Baht thaïlandais: THB",
-            "Couronne danoise: DKK",
-            "Couronne islandaise: ISK",
-            "Couronne norvégienne: NOK",
-            "Couronne suédoise: SEK",
-            "Couronne thcèque: CZK",
-            "Dollar américain: USD",
-            "Dollar australien: AUD",
-            "Dollar canadien: CAD",
-            "Dollar de Hong Kong: HKD",
-            "Dollar néo-zélandais: NZD",
-            "Dolar singapourien: SGD",
-            "Euro: EUR",
-            "Forint hongrois: HUF",
-            "Franc suisse: CHF",
-            "Kuna croate: HRK",
-            "Leu roumain: RON",
-            "Lev bulgare: BGN",
-            "Livre sterling: GBP",
-            "Livre turque: TRY",
-            "Peso mexicain: MXN",
-            "Peso philippin: PHP",
-            "Rand sud-africain: ZAR",
-            "Real brésilien: BRL",
-            "Ringgit malaisien: MYR",
-            "Rouble russe: RUB",
-            "Roupie indienne: INR",
-            "Roupie indonésienne: IDR",
-            "Shekel israélien: ILS",
-            "Won de la Corée du Sud: KRW",
-            "Yen japonais: JPY",
-            "Yuan Renminbi chinois: CNY",
-            "Zloty polonais: PLN"
+    "Baht thaïlandais: THB",
+    "Couronne danoise: DKK",
+    "Couronne islandaise: ISK",
+    "Couronne norvégienne: NOK",
+    "Couronne suédoise: SEK",
+    "Couronne thcèque: CZK",
+    "Dollar américain: USD",
+    "Dollar australien: AUD",
+    "Dollar canadien: CAD",
+    "Dollar de Hong Kong: HKD",
+    "Dollar néo-zélandais: NZD",
+    "Dolar singapourien: SGD",
+    "Euro: EUR",
+    "Forint hongrois: HUF",
+    "Franc suisse: CHF",
+    "Kuna croate: HRK",
+    "Leu roumain: RON",
+    "Lev bulgare: BGN",
+    "Livre sterling: GBP",
+    "Livre turque: TRY",
+    "Peso mexicain: MXN",
+    "Peso philippin: PHP",
+    "Rand sud-africain: ZAR",
+    "Real brésilien: BRL",
+    "Ringgit malaisien: MYR",
+    "Rouble russe: RUB",
+    "Roupie indienne: INR",
+    "Roupie indonésienne: IDR",
+    "Shekel israélien: ILS",
+    "Won de la Corée du Sud: KRW",
+    "Yen japonais: JPY",
+    "Yuan Renminbi chinois: CNY",
+    "Zloty polonais: PLN",
 ]
+
 
 def prompt_processor(input_string):
     if input_string == "help":
-        return(
+        return (
             "[taux] [devise départ] [devise cible]\n"
             "`list` pour avoir une liste des devises\n"
             "Certaines conversions ne pourrait pas marcher car il n'y pas de "
             "données disponibles."
         )
-        return ("[taux] [devise départ] [devise cible]\n"
-        "'list' pour avoir une liste des devises'")
+        return (
+            "[taux] [devise départ] [devise cible]\n"
+            "'list' pour avoir une liste des devises'"
+        )
     money_list = [value for value in input_string.split(" ")]
 
     if input_string == "list":
@@ -73,6 +78,7 @@ def prompt_processor(input_string):
 
     final = f"{cr.convert(currency, convert, amount)} {convert}\n"
     return final
+
 
 print("Convertisseur de monnaie :\n")
 
